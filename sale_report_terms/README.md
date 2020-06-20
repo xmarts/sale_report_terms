67a8644c25472e9adc801e226449e8ebb382f45e
BeopenMX <manuel9410z@gmail.com>
2020-06-20 14:49:01 -0500

    Add files via upload

---
+++ sale_report_terms/README.md
@@ -0,0 +1,10 @@
+Xmarts
+----
+
+<h3>sale_report_terms </h3>
+
+Funcion:
+<table>
+<tr>Este modulo tiene la siguiente funcion:
+<td> 
+Agrega un campo en res_company para tener unos terminos de sale order globales 
---
+++ sale_report_terms/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Informe sales terminos',
+    'version': '13.0.0',
+    'summary': 'Agrega un apartado en compania para definir terminos del sales order',
+    'description': 'Agrega un apartado en compania para definir terminos del sales order',
+    'author': 'Xmarts',
+    'website': 'www.xmarts.com',
+    'depends': ['base','sale'],
+    'data': [
+        'views/xmarts_config.xml',
+        'reports/reports.xml',
+    ],
+    'installable': True,
+}
